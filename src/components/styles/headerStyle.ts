a391b144f49c30a5efdcace12463312d103bcb38
Ruben Rodriguez <rrodriguez6@outlook.com>
2026-08-07 12:31:32 -0500

    Astro rewrite using Claude (#31)

--- src/components/styles/headerStyle.ts
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components"
-
-export const Headerline = styled.span`
-  font-weight: bold;
-`
---
+++ src/data/site.ts
@@ -0,0 +1,7 @@
+// Site-wide metadata (previously gatsby-config.ts siteMetadata).
+export const siteMetadata = {
+  title: "Alexandra Tate",
+  description: "Sociologist Dr. Alexandra Tate",
+  twitterUsername: "@cestmoialex",
+  siteUrl: "https://www.alexandra-tate.com",
+};
